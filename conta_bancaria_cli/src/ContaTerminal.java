3cd75a1586a6e2fbd82351512d78df7e14a05046
Dum2601 <Doug_b_souza@hotmail.com>
2026-02-11 22:09:53 -0300

    update: main account logic working

--- conta_bancaria_cli/src/ContaTerminal.java
+++ conta_bancaria_cli/src/ContaTerminal.java
@@ -1,8 +1,44 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
 
     public static void main(String[] args) throws Exception {
 
-        System.out.println("Iniciando o projeto");
+        Conta teste = new Conta();
+        System.out.println(teste.getBalance());
 
+    }
+}
+
+
+
+class Conta {
+    private Scanner readKey = new Scanner(System.in);
+    private double saldo;
+    private double valorSolicitado;
+
+    public Conta() {
+        this.saldo = 25.0;
+        this.valorSolicitado = 18.0;
+    }
+
+    private boolean balanceCheck(double valorSolicitado) {
+        if (this.saldo > valorSolicitado) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void setSaldo(double valorSolicitado) {
+        if (this.balanceCheck(valorSolicitado)){
+            this.saldo = this.saldo - valorSolicitado;
+        } else {
+            System.out.println("Saldo insuficiente");
+        }
+    }
+
+    public double getBalance() {
+        return this.saldo -= this.valorSolicitado;
     }
 }
